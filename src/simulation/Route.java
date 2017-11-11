1e1432c2d517979ac52a908d72a351d5e9d5c9e4
kkrol27 <kyle.krol.27@gmail.com>
2017-11-10 20:28:48 -0500

    More comments

--- src/simulation/Route.java
+++ src/simulation/Route.java
@@ -2,14 +2,28 @@ package simulation;
 
 import java.util.List;
 
+/**
+ * Represents a general bus route. A bus route is made of of a series of route
+ * elements.
+ */
 public class Route {
 
+	/** List of route elements */
 	private List<Element> elements;
 	
+	/**  */
 	Route(List<Element> elements) {
 		this.elements = elements;
 	}
 	
+	/**
+	 * Drives the bus through a route a specified number of times.
+	 * 
+	 * @param bus
+	 * 		the bus driving through the route
+	 * @param n
+	 * 		the number of times the bus will drive the route
+	 */
 	public void drive(Bus bus, int n) {
 		for(int i = 0; i < n; i++)
 			drive(bus);
