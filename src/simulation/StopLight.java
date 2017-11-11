d6762a93f04bf5f9fc698a99a2e06f4e9b406c06
kkrol27 <kyle.krol.27@gmail.com>
2017-11-11 10:29:01 -0500

    comments

--- src/simulation/StopLight.java
+++ src/simulation/StopLight.java
@@ -12,6 +12,18 @@ public class StopLight extends WaitElement {
 	/** Speed the bus travels through the stop light */
 	private double throughSpeed;
 
+	/**
+	 * Creates a new stop light with the specified congestion factor and
+	 * through speed. The congestion factor will scale the standard stop light
+	 * wait time to give this particular light a custom wait time. The through
+	 * speed represents how fast a bus will cross this light if the light is
+	 * green when the bus approaches.
+	 * 
+	 * @param congestion
+	 * 		the congestion factor to scale the standard wait time
+	 * @param throughSpeed
+	 * 		the through speed of this light
+	 */
 	StopLight(double congestion, double throughSpeed) {
 		super(congestion);
 		this.throughSpeed = throughSpeed;
