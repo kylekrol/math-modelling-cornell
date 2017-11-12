06aac5be07e61c77d89b0009e612a676eb7952ec
kkrol27 <kyle.krol.27@gmail.com>
2017-11-12 12:29:32 -0500

    drive updates

--- src/simulation/buses/Bus.java
+++ src/simulation/buses/Bus.java
@@ -28,7 +28,7 @@ public class Bus {
 	/** Updates gas usage given an amount of energy e consumed in joules */
 	private void runEngine(double e) {
 		
-		System.out.println(Constants.GAS_PER_JOULE * e / Constants.BUS_EFF);
+		// System.out.println(Constants.GAS_PER_JOULE * e / Constants.BUS_EFF);
 		
 		if(e >= 0)
 			gasUsage += (Constants.GAS_PER_JOULE * e / Constants.BUS_EFF > 1.0d ? 0.0d : Constants.GAS_PER_JOULE * e / Constants.BUS_EFF);
@@ -37,72 +37,53 @@ public class Bus {
 	}
 	
 	public void travel(double vf, double vlim, double dx, double dy) {
-		double dk, du, dw;
+		double dk1, dk2, du, dw;
 		double sin = dy / dx;
 		double ap = Constants.BUS_ACCEL - 9.81d * sin;
-		double am = - Constants.BUS_DECCEL - 9.81d * sin;
+		double am = -Constants.BUS_DECCEL - 9.81d * sin;
 		
 		// Check if vf is attainable
 		double a = (v > vf ? am : ap);
-		double dxmin = (vf*vf - v*v) / 2*a1;
+		double dxmin = (vf*vf - v*v) / 2*a;
 		if(dxmin >= dx) {
 			// vf is not attainable on this leg
 			double vend = Math.sqrt(v*v + 2*a*dx);
-			dk = 0.5d * Constants.BUS_MASS * (vend*vend - v*v);
+			dk1 = 0.5d * Constants.BUS_MASS * (vend*vend - v*v);
 			du = Constants.BUS_MASS * 9.81d * dy;
-			dw = workRes(a, v, dx);
+			dw = workRes((a > 0 ? Constants.BUS_ACCEL : -Constants.BUS_DECCEL), v, dx);
 			v = vend;
 		} else {
 			// vf is not attainable
 			double a1 = (v > vlim ? am : ap);
 			double a2 = (vf < vlim ? am : ap);
-			double x1 = (vlim*vlim - v*v) / 2*a1;
-			double x2 = (vf*vf - vlim*vlim) / 2*a2;
-			if(x1 + x2 <= dx) {
+			double dx1 = (vlim*vlim - v*v) / 2*a1;
+			double dx2 = (vf*vf - vlim*vlim) / 2*a2;
+			if(dx1 + dx2 <= dx) {
 				// Speed limit can be attained
-				
+				dk1 = 0.5d * Constants.BUS_MASS * (vlim*vlim - v*v);
+				dk2 = 0.5d * Constants.BUS_MASS * (vf*vf - vlim*vlim);
+				du = Constants.BUS_MASS * 9.81d * dy;
+				dw = workRes((a1 > 0 ? Constants.BUS_ACCEL : -Constants.BUS_DECCEL), v, dx1) + 
+					    workRes(0.0d, vlim, dx - dx1 - dx2) + 
+					    workRes((a2 > 0 ? Constants.BUS_ACCEL : -Constants.BUS_DECCEL), vlim, dx2);
+				v = vf;
 			} else {
-				
+				// Speed limit cannot be attained
+				double vmax = Math.sqrt( (2*a1*a2*dx + a2*v*v - a1*vf*vf) / (a2 - a1) );
+				dx1 = (vmax*vmax - v*v) / 2*ap;
+				dx2 = (vf*vf - vmax*vmax) / 2*am;
+				dk1 = 0.5d * Constants.BUS_MASS * (vmax*vmax - v*v);
+				dk2 = 0.5d * Constants.BUS_MASS * (vf*vf - vmax*vmax);
+				du = Constants.BUS_MASS * 9.81d * dy;
+				dw = workRes((a1 > 0 ? Constants.BUS_ACCEL : -Constants.BUS_DECCEL), v, dx1) + 
+					    workRes(0.0d, vmax, dx - dx1 - dx2) + 
+					    workRes((a2 > 0 ? Constants.BUS_ACCEL : -Constants.BUS_DECCEL), vmax, dx2);
+				v = vf;
 			}
 		}
-		runEngine(dk + du + dw);
-		
-		double dxp = (vlim*vlim - v*v) / 2*ap;
-		double dxm = (vf*vf - vlim*vlim) / 2*am;
-		
-		if(dxp > dx) {
-			System.out.println("Assertion");
-		}
-		
-		double dxmin = dxp + dxm;
-		
-		if(dxmin <= dx) {
-			// Desired vf is possible while attaining vlim
-			double dk = 0.5d * Constants.BUS_MASS * (vlim*vlim - v*v);
-			double du = Constants.BUS_MASS * 9.81d * dy;
-			double dw = workRes(ap, v, dxp) + 
-					    workRes(0.0d, vlim, dx - dxp - dxm) + 
-					    workRes(am, vlim, dxm);
-			
-			//System.out.println("k from 1st: " + dk);
-			
-			runEngine(dk + du + dw);
-		} else {
-			// Desired vf cannot be reached
-			double vmax = Math.sqrt( (2*am*ap*dx + am*v*v - ap*vf*vf) / (am - ap) );
-			dxp = (vmax*vmax - v*v) / 2*ap;
-			dxm = (vf*vf - vmax*vmax) / 2*am;
-			
-			double dk = 0.5d * Constants.BUS_MASS * (vlim*vlim - v*v);
-			double du = Constants.BUS_MASS * 9.81d * dy;
-			double dw = workRes(ap, v, dxp) + 
-					    workRes(0.0d, vmax, dx - dxp - dxm) + 
-					    workRes(am, vmax, dxm);
-			
-			//System.out.println("k from 2nd: " + dk);
-			
-			runEngine(dk + du + dw);
-		}
+		runEngine(dk1);
+		runEngine(dk1);
+		runEngine(du + dw);
 	}
 	
 	/**
